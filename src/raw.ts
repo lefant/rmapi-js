c696c9cd0cab16aee1033ed73bd9ef7ae89aac0c
Fabian Linzberger <e@lefant.net>
2026-01-08 17:12:00 +0100

    allow nullable cPages.uuids in schema instead of normalising to array

--- src/raw.ts
+++ src/raw.ts
@@ -276,7 +276,7 @@ export interface CPages {
   /** [speculative] information about individual pages */
   pages: CPagePage[];
   /** [unknown] */
-  uuids: CPageUUID[];
+  uuids: CPageUUID[] | null;
 }
 
 const cPages = properties(
@@ -298,14 +298,16 @@ const cPages = properties(
       true,
     ),
     pages: elements(cPagePage),
-    uuids: elements(
-      properties(
-        {
-          first: string(),
-          second: uint32(),
-        },
-        undefined,
-        true,
+    uuids: nullable(
+      elements(
+        properties(
+          {
+            first: string(),
+            second: uint32(),
+          },
+          undefined,
+          true,
+        ),
       ),
     ),
   },
@@ -1070,16 +1072,6 @@ export class RawRemarkable implements RawRemarkableApi {
   async getContent(hash: string): Promise<Content> {
     const raw = await this.getText(hash);
     const loaded = JSON.parse(raw) as unknown;
-
-    // Normalize fields for documents in special states
-    if (loaded && typeof loaded === 'object' && 'fileType' in loaded) {
-      // Transform null cPages.uuids to empty array (certain document states)
-      if ('cPages' in loaded && loaded.cPages && typeof loaded.cPages === 'object') {
-        if ('uuids' in loaded.cPages && loaded.cPages.uuids === null) {
-          (loaded.cPages as any).uuids = [];
-        }
-      }
-    }
 
     // jtd can't verify non-discriminated unions, in this case, we have fileType
     // defined or not. As a result, we try each, and concatenate the errors at the end
